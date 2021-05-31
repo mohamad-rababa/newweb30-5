1e4176e63b60095420a8a835c809f4a14c02179e
MohamadRashed <>
2021-05-31 12:54:47 +0000

    31/5

--- mo.js
+++ mo.js
@@ -1,10 +1,8 @@
 alert("Hello")
-// var age =prompt("please enter your favourite number");
-// if (number >=0 || number)
 
-// var txt;
-var r = prompt("what your favourite team, please enter barca or faisaly ");
 
+function validateinputuser (){
+  var r = prompt("what your favourite team, please enter barca or faisaly ");
 while (r!="barca" || r!="faisaly")
 {if (r == 'barca')
 {
@@ -16,11 +14,19 @@ while (r!="barca" || r!="faisaly")
     }
   }
 
+}
+ validateinputuser ()
 
 
-var rate =prompt ('how many star rating would you give this content, 1-5?');
+function validate (){
+  var rate =prompt ('how many star rating would you give this content, 1-5?');
 
 
 for(var i=0;i<rate;i++)
 { document.write('<img src="https://media.gettyimages.com/photos/lionel-messi-of-barcelona-gestures-during-the-uefa-champions-league-picture-id1143242800?k=6&m=1143242800&s=612x612&w=0&h=Pc1h5X5H4Fft3sFUSa5Mv7-iETFSp1kr2OfAaiA8yH8="> ', i)
 }
+
+
+
+}
+validate ()
